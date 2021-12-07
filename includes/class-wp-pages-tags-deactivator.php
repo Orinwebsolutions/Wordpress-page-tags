60876f775d5e413e8cfbf0d3be995fa536418e01
evicio-dev <amila.p@evicio.com>
2021-12-07 21:18:29 +0530

    initial commits

---
+++ includes/class-wp-pages-tags-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/Orinwebsolutions
+ * @since      1.0.0
+ *
+ * @package    Wp_Pages_Tags
+ * @subpackage Wp_Pages_Tags/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Pages_Tags
+ * @subpackage Wp_Pages_Tags/includes
+ * @author     Amila Priyankara <amilapriyankara16@gmail.com>
+ */
+class Wp_Pages_Tags_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
